1e1be1246300dfbd169692946960deb272f1d980
Denys Prostybozhenko <denisprost704@gmail.com>
2026-04-22 16:46:54 +0300

    final tasks

--- src/app/api/secret/route.ts
+++ src/app/api/secret/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
+import prisma from "@/lib/db";
 
 export async function GET() {
+  const posts = await prisma.testPost.findMany();
   console.log(process.env.SECRET_WORD);
-  return NextResponse.json({});
+  return NextResponse.json(posts);
 }
---
+++ src/lib/db.ts
@@ -0,0 +1,11 @@
+import "server-only";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+  }),
+});
+
+export default prisma;
